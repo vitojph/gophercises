9e1b418deeef3ed868e3eeb86b8952b4d8e75a35
Víctor Peinado <vitojph@gmail.com>
2020-05-03 20:45:33 +0200

    Add new functionalities: templates and paths

--- cyoa/story.go
+++ cyoa/story.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"html/template"
 	"io"
+	"log"
 	"net/http"
+	"strings"
 )
 
 // JSONStory parses a JSON file containing a story
@@ -22,34 +24,71 @@ type Story map[string]Chapter
 
 // Chapter is the main item of a story
 type Chapter struct {
-	Title string   `json:"title"`
+	Title      string   `json:"title"`
 	Paragraphs []string `json:"story"`
-	Options []Option `json:"options"`
+	Options    []Option `json:"options"`
 }
 
 // Option contains the availabe choices
 type Option struct {
-	Text string `json:"text"`
+	Text    string `json:"text"`
 	Chapter string `json:"arc"`
 }
 
-// NewHandler manages the story
-func NewHandler(s Story) http.Handler {
-	return handler{s}
-}
+// HandlerOption is used to configure the http.Handler returned
+type HandlerOption func(h *handler)
 
-type handler struct {
-	s Story
-}
-
-func (h handler) ServeHTTP(w http.ResponseWriter, r  *http.Request) {
-	err := tpl.Execute(w, h.s["intro"])
-	if err != nil {
-		panic(err)
+// WithTemplate is an option to provide custom template to render stories
+func WithTemplate(t *template.Template) HandlerOption {
+	return func(h *handler) {
+		h.t = t
 	}
 }
 
-func init() {	
+func WithPathFunc(fn func(r *http.Request) string) HandlerOption {
+	return func(h *handler) {
+		h.pathFn = fn
+	}
+}
+
+// NewHandler manages the story
+func NewHandler(s Story, opts ...HandlerOption) http.Handler {
+	h := handler{s, tpl, defaultPathFn}
+	for _, opt := range opts {
+		opt(&h)
+	}
+	return h
+}
+
+type handler struct {
+	s      Story
+	t      *template.Template
+	pathFn func(r *http.Request) string
+}
+
+func defaultPathFn(r *http.Request) string {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "" || path == "/" {
+		path = "/intro"
+	}
+	return path[1:]
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := h.pathFn(r)
+
+	if chapter, ok := h.s[path]; ok {
+		err := h.t.Execute(w, chapter)
+		if err != nil {
+			log.Printf("%v", err)
+			http.Error(w, "Something went wrong...", http.StatusInternalServerError)
+		}
+		return
+	}
+	http.Error(w, "Chapter not found.", http.StatusNotFound)
+}
+
+func init() {
 	tpl = template.Must(template.New("").Parse(defaultHandlerTemplate))
 }
 
@@ -57,20 +96,65 @@ var tpl *template.Template
 
 var defaultHandlerTemplate = `<!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>Choose Your Own Adventure</title>
-    </head>
-    <body>
-    <h1>{{.Title}}</h1>
-    {{range .Paragraphs}}
-    <p>{{.}}</p>
-    {{end}}
-
-    <ul>
+  <head>
+    <meta charset="utf-8">
+    <title>Choose Your Own Adventure</title>
+  </head>
+  <body>
+    <section class="page">
+      <h1>{{.Title}}</h1>
+      {{range .Paragraphs}}
+        <p>{{.}}</p>
+      {{end}}
+      {{if .Options}}
+        <ul>
         {{range .Options}}
-        <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+          <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
         {{end}}
-    </ul>
-    </body>
+        </ul>
+      {{else}}
+        <h3>The End</h3>
+      {{end}}
+    </section>
+    <style>
+      body {
+        font-family: helvetica, arial;
+      }
+      h1 {
+        text-align:center;
+        position:relative;
+      }
+      .page {
+        width: 80%;
+        max-width: 500px;
+        margin: auto;
+        margin-top: 40px;
+        margin-bottom: 40px;
+        padding: 80px;
+        background: #FFFCF6;
+        border: 1px solid #eee;
+        box-shadow: 0 10px 6px -6px #777;
+      }
+      ul {
+        border-top: 1px dotted #ccc;
+        padding: 10px 0 0 0;
+        -webkit-padding-start: 0;
+      }
+      li {
+        padding-top: 10px;
+      }
+      a,
+      a:visited {
+        text-decoration: none;
+        color: #6295b5;
+      }
+      a:active,
+      a:hover {
+        color: #7792a2;
+      }
+      p {
+        text-indent: 1em;
+      }
+    </style>
+  </body>
 </html>`
