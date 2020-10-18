e94f871dccd2a2f94e0560b15c8b1177dd8985ef
Víctor Peinado <vitojph@gmail.com>
2020-10-18 18:48:36 +0200

    Add non recursive renaming

--- renamer/main.go
+++ renamer/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 )
 
 func main() {
-	fileName := "birthday_001.txt"
+	/* fileName := "birthday_001.txt"
 
 	newName, err := match(fileName, 4)
 
@@ -16,14 +18,47 @@ func main() {
 		fmt.Println("No match")
 		os.Exit(1)
 	}
-	fmt.Println(newName)
+	fmt.Println (newName)*/
+	dir := "./sample"
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+
+	count := 0
+	var toRename []string
+
+	for _, file := range files {
+		if file.IsDir() {
+			fmt.Println("Dir: %s", file.Name())
+		} else {
+			_, err := match(file.Name(), 4)
+			if err == nil {
+				count++
+				toRename = append(toRename, file.Name())
+			}
+		}
+	}
+	for _, origFilename := range toRename {
+		origPath := filepath.Join(dir, origFilename)
+		newFileName, err := match(origFilename, count)
+		if err != nil {
+			panic(err)
+		}
+		newPath := filepath.Join(dir, newFileName)
+		fmt.Printf("mv %s %s\n", origPath, newPath)
+		err = os.Rename(origPath, newPath)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
 
 // match returns the new file name, or an error if the file name
 // didn't match the pattern
-func match(fileName string, total int) (string, error) {
+func match(filename string, total int) (string, error) {
 	// get the extension
-	pieces := strings.Split(fileName, ".")
+	pieces := strings.Split(filename, ".")
 	ext := pieces[len(pieces)-1]
 	tmp := strings.Join(pieces[0:len(pieces)-1], ".")
 
@@ -32,7 +67,7 @@ func match(fileName string, total int) (string, error) {
 
 	number, err := strconv.Atoi(pieces[len(pieces)-1])
 	if err != nil {
-		return "", fmt.Errorf("%s didn't match our pattern", fileName)
+		return "", fmt.Errorf("%s didn't match our pattern", filename)
 	}
 	return fmt.Sprintf("%s - %d of %d.%s", strings.Title(name), number, total, ext), nil
 }
